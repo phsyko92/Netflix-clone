88843fa9ca140e02f4c22a0b693d4ddea9439610
julienfonteneau <julienfonteneau@hotmail.com>
2024-12-04 18:45:36 +0100

    make the home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,8 @@
 import Navbar from '../../components/Navbar/Navbar.jsx';
+import hero_banner from '../../assets/hero_banner.jpg';
+import hero_title from '../../assets/hero_title.png';
+import play_icon from '../../assets/play_icon.png';
+import info_icon from '../../assets/info_icon.png';
 
 import './Home.css'
 
@@ -6,6 +10,20 @@ const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <div className='hero'>
+        <img src={hero_banner} alt="" className='banner-img'/>
+        <div className='hero-caption'>
+          <img src={hero_title} alt='' className='caption-img'/>
+          <p>Discovering his ties to a scret ancient order, a young man
+            living in modern Istanbul embarks on a quest to save the
+            city from an immortel enemy.
+          </p>
+          <div className="hero-btns">
+            <button className='btn'><img src={play_icon} alt="" />Play</button>
+            <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 
