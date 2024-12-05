4a90e9f40b1133ffac664155422e5247edf57ed8
julienfonteneau <julienfonteneau@hotmail.com>
2024-12-05 18:18:35 +0100

    Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Home from './pages/Home/Home.jsx';
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/Login.jsx'
 
 function App() {
   return (
     <>
-      <Home/>
+      <Routes>
+        <Route path='/' element={ <Home/>}/>
+        <Route path='/login' element={ <Login/>}/>
+      </Routes>
+
     </>
   )
 }
