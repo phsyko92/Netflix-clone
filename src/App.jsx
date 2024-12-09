e04b9126ceefddcd12f0f4925d681141e5e616f7
julienfonteneau <julienfonteneau@hotmail.com>
2024-12-10 00:23:24 +0100

    credential and toast

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import { onAuthStateChanged } from 'firebase/auth';
 import { useEffect } from 'react';
 import { auth } from './firebase';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
 
   const navigate = useNavigate();
@@ -24,6 +27,7 @@ function App() {
 
   return (
     <>
+      <ToastContainer theme='dark'/>
       <Routes>
         <Route path='/' element={ <Home/>}/>
         <Route path='/login' element={ <Login/>}/>
